dd95183c221504891b6aec5d31dddecbdbeaec4b
YoussefElmoudene <elmoudene.ysf@gmail.com>
2024-01-15 12:08:24 +0100

    integrate kafka

---
+++ tax/src/main/java/com/example/tax/dto/User.java
@@ -0,0 +1,25 @@
+package com.example.tax.dto;
+
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class User implements Serializable {
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String username;
+    private String tel;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public String toString() {
+        return "User{" + "id=" + id + ", firstName='" + firstName + '\'' + ", lastName='" + lastName + '\'' + ", email='" + email + '\'' + ", password='" + password + '\'' + ", username='" + username + '\'' + ", tel='" + tel + '\'' + ", role=" + role + '}';
+    }
+}
