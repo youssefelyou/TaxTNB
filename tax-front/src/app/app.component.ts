220d7257d869579f6e0aa51ad9b8caecfd4096e2
YoussefElmoudene <elmoudene.ysf@gmail.com>
2024-01-15 14:29:22 +0100

    integrate websocket

--- tax-front/src/app/app.component.ts
+++ tax-front/src/app/app.component.ts
@@ -1,19 +1,31 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {WebSocketService} from "./controller/service/websocket.service";
+import {SocketIoConfig} from "ngx-socket-io";
+import {Subscription} from "rxjs";
+
+const config: SocketIoConfig = {url: 'ws://localhost:8080/websocket', options: {}};
 
 @Component({
-    selector   : 'app-root',
+    selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls  : ['./app.component.scss'],
-    standalone : true,
-    imports    : [RouterOutlet],
+    styleUrls: ['./app.component.scss'],
+    standalone: true,
+    imports: [RouterOutlet],
 })
-export class AppComponent
-{
+export class AppComponent {
+
+
+    receivedMessages: string[] = [];
+    private messageSubscription: Subscription;
+
     /**
      * Constructor
      */
-    constructor()
-    {
+    constructor(private webSocketService: WebSocketService) {
+        this.messageSubscription = this.webSocketService.getMessages().subscribe(message => {
+            console.log(message)
+            this.receivedMessages.push(message);
+        });
     }
 }
